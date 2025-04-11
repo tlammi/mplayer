7d6e726710f9ec1b76aabccc7c98bc14a3f25b7b
Toni Lammi <tangojulietlima@gmail.com>
2025-04-11 22:01:33 +0300

    first events

--- mplayer/schedule_doc.py
+++ mplayer/schedule_doc.py
@@ -1,11 +1,38 @@
 
 from dataclasses import dataclass
+from datetime import datetime, date, timedelta
+
+import tomli
+
+
+def _to_datetime(val: datetime | date):
+    if isinstance(val, datetime):
+        return val
+    return datetime.combine(val, datetime.min.time())
+
+@dataclass
+class Event:
+    playlist: str
+    at: datetime
+
+@dataclass
+class OffsetEvent:
+    playlist: str
+    offset: timedelta
+
 
 class ScheduleDoc(list):
     def __init__(self):
         pass
 
-
     @staticmethod
-    def from_obj(data: list) -> "ScheduleDoc":
-        pass
+    def from_obj(data: dict) -> "ScheduleDoc":
+        s = ScheduleDoc()
+        for obj in data.get("schedule", []):
+            s.append(Event(playlist=obj["playlist"], at=_to_datetime(obj["at"])))
+        return s
+
+    @classmethod
+    def from_str(cls, data: str):
+        return cls.from_obj(tomli.loads(data))
+
--- test/unit/test_schedule_doc.py
+++ test/unit/test_schedule_doc.py
@@ -1,8 +1,51 @@
+from datetime import datetime
 
 from mplayer.schedule_doc import ScheduleDoc
 
 
-def test_init_empty():
+def _dt_str(s: str):
+    return datetime.fromisoformat(s)
+
+
+def test_default():
     s = ScheduleDoc()
     assert len(s) == 0
+
+def test_from_str_empty():
+    s = ScheduleDoc.from_str("")
+    assert len(s) == 0
+
+def test_from_str_enums_only():
+    s = ScheduleDoc.from_str('playlists = ["foo", "bar"]')
+    assert len(s) == 0
+
+def test_from_str_one():
+    data = """
+[[schedule]]
+playlist = "foo"
+at = 2000-01-01
+"""
+    s = ScheduleDoc.from_str(data)
+    assert len(s) == 1
+    evt = s[0]
+    assert evt.playlist == "foo"
+    assert evt.at == _dt_str("2000-01-01")
+
+def test_from_str_two():
+    data = """
+[[schedule]]
+playlist = "foo"
+at = 2000-01-01 00:00:00
+
+[[schedule]]
+playlist = "bar"
+at = 2000-01-01 01:00:00
+"""
+    s = ScheduleDoc.from_str(data)
+    assert len(s) == 2
+    foo, bar = s
+    assert foo.playlist == "foo"
+    assert bar.playlist == "bar"
+    assert foo.at == _dt_str("2000-01-01T00:00:00")
+    assert bar.at == _dt_str("2000-01-01T01:00:00")
 
